b8d989ad1fd7b101c13fa83536dcc64798a6cc17
patricou <deschamps.pat@gmail.com>
2025-10-28 22:08:02 +0100

    20251028

--- PatTool_Back-End/src/main/java/com/pat/config/WebConfig.java
+++ PatTool_Back-End/src/main/java/com/pat/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.pat.config;
 import com.pat.converter.StringToMemberConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,5 +16,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToMemberConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward all non-API routes to Angular's index.html
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
     }
 }
