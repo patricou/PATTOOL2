88ac5f4fd89f5c498f45a8d130f4c2d83fa65421
patricou <deschamps.pat@gmail.com>
2026-01-27 23:06:23 +0100

    CSP-correction

--- PatTool_Back-End/src/main/java/com/pat/config/SecurityConfig.java
+++ PatTool_Back-End/src/main/java/com/pat/config/SecurityConfig.java
@@ -110,7 +110,7 @@ public class SecurityConfig {
                 // font-src: Allow Google Fonts (fonts.gstatic.com) and Font Awesome (maxcdn.bootstrapcdn.com)
                 // img-src: Allow blob: for Angular image handling
                 // media-src: Allow blob: for video compression and playback
-                // connect-src: Allow source maps, Firebase, and Keycloak connections
+                // connect-src: Allow source maps, Firebase, Keycloak, and Nominatim (OpenStreetMap) connections
                 .contentSecurityPolicy(csp -> csp
                     .policyDirectives("default-src 'self'; " +
                         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://sportpat-5e155.firebaseio.com https://*.firebaseio.com https://apis.google.com https://*.googleapis.com https://*.gstatic.com https://www.gstatic.com https://www.googleapis.com; " +
@@ -118,7 +118,7 @@ public class SecurityConfig {
                         "img-src 'self' data: https: blob:; " +
                         "media-src 'self' data: https: blob:; " +
                         "font-src 'self' data: https://fonts.gstatic.com https://maxcdn.bootstrapcdn.com; " +
-                        "connect-src 'self' blob: http://localhost:8080 http://localhost:8000 https://www.patrickdeschamps.com:8543 https://cdn.jsdelivr.net https://sportpat-5e155.firebaseio.com https://*.firebaseio.com https://sportpat-5e155.firebaseapp.com https://*.firebaseapp.com https://*.googleapis.com https://www.googleapis.com https://*.gstatic.com https://www.gstatic.com wss://sportpat-5e155.firebaseio.com wss://*.firebaseio.com ws://localhost:8000 http://localhost:8000/ws; " +
+                        "connect-src 'self' blob: http://localhost:8080 http://localhost:8000 https://www.patrickdeschamps.com:8543 https://cdn.jsdelivr.net https://sportpat-5e155.firebaseio.com https://*.firebaseio.com https://sportpat-5e155.firebaseapp.com https://*.firebaseapp.com https://*.googleapis.com https://www.googleapis.com https://*.gstatic.com https://www.gstatic.com https://nominatim.openstreetmap.org wss://sportpat-5e155.firebaseio.com wss://*.firebaseio.com ws://localhost:8000 http://localhost:8000/ws; " +
                         "frame-src 'self' https://www.patrickdeschamps.com:8543 http://localhost:8080 https://*.firebaseio.com https://sportpat-5e155.firebaseio.com https://www.google.com https://maps.google.com https://*.google.com;")
                 )
                 // Note: frameOptions is not set to allow Keycloak iframes
